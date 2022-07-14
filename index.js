bc2ad9a61baa0d26837bf4679d36f9b634162734
hasan-furkan <81443587+hasan-furkan@users.noreply.github.com>
2022-07-14 16:48:26 +0300

    commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, Intents, Collection } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+});
+
+client.login(token);
